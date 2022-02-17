b3afe8511b832d3b9a84a90846d820271e192eed
JooyoungHwnag <joodudsdx@gmail.com>
2022-02-18 00:57:29 +0900

    Add: mixin 세팅

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageSample from '@pages/PageSample';
+import ComponentSample from '@components/ComponentSample';
 
 function main() {
 	return (
 		<div className="main">
 			<PageSample />
+			<ComponentSample />
 		</div>
 	);
 }
---
+++ src/styles/theme.tsx
@@ -0,0 +1,21 @@
+const theme = {
+	background: '#000',
+	mainColor: '#1a237e',
+	white: '#FFFFFF',
+	mainGrey: '#gray',
+	fontColor: '#2D2B2B',
+	fontLarge: '38px',
+	fontSemiLarge: '28px',
+	fontMedium: '22px',
+	fontRegular: '14px',
+	fontSmall: '12px',
+	fontWeightBold: '700',
+	fontWeightSemiBold: '500',
+	fontWeightRegular: '400',
+	fontWeightLight: '200',
+	flexCenterCenter: 'display:flex; justify-content:center; align-items:center;',
+	flexSpaceBetweenCenter: 'display:flex; justify-content:space-between; align-items:center;',
+	flexColumnCenter: 'display:flex; flex-direction:column; justify-content:center; align-items:center ',
+};
+
+export default theme;
