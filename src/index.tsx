be0d915ab579288edaf10257b7f784bcacd49b38
JooyoungHwnag <joodudsdx@gmail.com>
2022-02-17 23:19:52 +0900

    Add: 스타일컴포넌트 추가 목데이터 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import ReactDOM from 'react-dom';
+import GlobalStyle from './styles/GlobalStyle';
 import Router from './Router';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-	<Provider store={store}>
+	<>
+		<GlobalStyle />
 		<Router />
-	</Provider>,
-
+	</>,
 	document.getElementById('root'),
 );
