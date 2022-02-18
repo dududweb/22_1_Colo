c8b5d59f58cc7daa0c4d471fdd04e7055f284b93
JooyoungHwnag <joodudsdx@gmail.com>
2022-02-18 11:28:06 +0900

    필터 상수데이터 추가

---
+++ src/components/side/SideData.ts
@@ -0,0 +1,8 @@
+export const RELEASE_DATA = {
+	comp_name: { title: '출고 요청업체', selectItems: ['콜로상사'] },
+	user_name: { title: '출고 요청자', selectItems: ['test_sample@collo.kr'] },
+	delivery_type: { title: '출고형식', selectItems: ['택배'] },
+	release_status: { title: '출고상태', selectItems: ['출고요청', '출고중', '출고완료'] },
+	release_type: { title: '출고유형', selectItems: ['오전(10:00) 출고', '오후(04:00) 출고'] },
+	file_name: 'Iamgold Corporation.xls',
+};
